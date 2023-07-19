2bd41da95557bbc7eb60406503774cd6f6e5ef32
Joe Lee <eojeel@users.noreply.github.com>
2023-07-19 20:27:17 +0100

    styling / start to vin form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -179,8 +179,10 @@
             </div>
         </div>
     </div>
-    <button type="submit">Submit</button>
-    </form>
+    <div class="flex justify-center mt-2">
+    <button class="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" type="submit">Submit</button>
+    </div>
+</form>
 
 </body>
 @vite('resources/js/app.js')
